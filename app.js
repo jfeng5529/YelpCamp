529e58b8baaa70e807abab6a55abc084f1aa1d13
Jane Feng <jfeng5529@bths.edu>
2020-05-24 20:17:41 +0000

    mongo altas

--- app.js
+++ app.js
@@ -56,6 +56,8 @@ app.use("/campgrounds", campgroundRoutes);
 
 
 
-app.listen(3000, function(){
-   console.log("The YelpCamp Server Has Started!");
-});
+// app.listen(3000, function(){
+//    console.log("The YelpCamp Server Has Started!");
+// });
+
+app.listen(process.env.PORT || 5000)
